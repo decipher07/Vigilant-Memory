9f745b648e7851dd502cf35a350642c5455b7abb
DJ <social.deej@gmail.com>
2022-04-14 22:16:16 +0530

    addons: assets and general view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import { Footer, Loader, Navbar, Services, Transactions, Welcome } from './components'
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello World
-      </h1>
+    <div className='min-h-screen'>
+      
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+
+      <Services />
+      <Transactions />
+      <Footer />
+
     </div>
   )
 }
