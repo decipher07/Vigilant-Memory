5df2363993baf4df2d33221366c2e3f73c455580
DJ <social.deej@gmail.com>
2022-04-15 21:18:15 +0530

    final push: change home page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,6 +3,7 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from './';
 import React, { useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext'
 
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
@@ -21,11 +22,18 @@ const commonStyles = () => 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex just
 
 const Welcome = () => {
 
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
-    const connectWallet = () => {
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
+        e.preventDefault();
+
+        if (!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
     }
-
+    
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -38,11 +46,11 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the Crypto world. Buy and sell cryptocurrencies easily on crypto our wallet
                     </p>
-                    <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer">
-                        <p className="text-white text-base font-semibold">
-                            Connect Wallet
-                        </p>
-                    </button>
+                    {/* Button Connecting Wallet and State */}
+                    {!currentAccount && (<button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer">
+
+                        <p className="text-white text-base font-semibold">Connect Wallet</p>
+                    </button>)}
 
                     {/* Table Grid */}
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
@@ -96,6 +104,19 @@ const Welcome = () => {
                         <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => { }} />
                         <Input placeholder="Keyword (gif)" name="keyword" type="text" handleChange={() => { }} />
                         <Input placeholder="Enter Message" name="message" type="text" handleChange={() => { }} />
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">
+                                Send Now
+                            </button>
+                        )}
+
                     </div>
 
                 </div>
