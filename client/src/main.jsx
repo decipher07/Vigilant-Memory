df8c8509c67ad017555e2e678d7301982a8ad88d
DJ <social.deej@gmail.com>
2022-04-15 19:56:14 +0530

    bug: ReactDom import library and dotenv in constants

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import TransactionContext from './context/TransactionContext'
+import { TransactionProvider } from './context/TransactionContext'
 
 ReactDOM.render(
   <TransactionProvider>
--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
 import  abi from './Transactions.json'
 
-export const contractAddress = process.env.CONTRACTADDRESS;
+export const contractAddress = "0x260DC38758f56004209f820576d4f91ff425B39b";
 export const contractABI = abi.abi;
